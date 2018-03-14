f284a800e117c58a50a29fd93944b4f388468609
dowenb00 <34041313+dowenb00@users.noreply.github.com>
2018-03-14 16:27:12 -0500

    Add project files.

---
+++ ConsoleApplication11/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string result = "";
+            int n = 20;
+            int n2 = 1;
+            for (int i = 0; i < n; i++)
+            {
+                for (int x = n2; x <= n; x++)
+                {
+                    result += x.ToString() + ",";
+                }
+                n2++;
+                result += "\n";
+            }
+            Console.WriteLine(result.Trim());
+        }
+    }
+}
